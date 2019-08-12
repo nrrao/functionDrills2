60ab41bf821816b61d1e8b0de233b054f83b8fff
nrrao <nandanarao@yahoo.com>
2019-08-12 18:18:20 -0400

    Jedi Name

---
+++ nandanaFunctiondrills2.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line strict
+function jediName(firstname,lastName){
+  let result =lastName.substring(0,3)+firstname.substring(0,2) ;
+  return result;
+
+    
+}
+
+console.log(jediName ('Beyonce','Knowels'));
